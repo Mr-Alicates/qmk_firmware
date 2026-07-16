ad5e21b1bffe038219694e3e54bf8db418959320
Mr-Alicates <pabtorca@gmail.com>
2026-07-16 22:14:27 +0200

    Add blinking layers

--- keyboards/ymdk/np21/keymaps/Mr_Alicates/keymap.c
+++ keyboards/ymdk/np21/keymaps/Mr_Alicates/keymap.c
@@ -24,46 +24,72 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// Default keymap
-//    [_BASE] = LAYOUT_ortho_6x4(
-//        KC_ESC,  KC_TAB,  KC_BSPC, MO(_LIGHTS),
-//        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-//        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-//        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-//        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-//        KC_P0,   KC_DOT,  KC_PDOT, KC_PENT
-//    ),
-//
-//    [_LIGHTS] = LAYOUT_ortho_6x4(
-//        _______, _______, _______, _______,
-//        _______, _______, _______, _______,
-//        _______, BL_ON,   _______, BL_UP, 
-//        _______, BL_TOGG, _______, BL_UP, 
-//        _______, BL_OFF,  _______, BL_DOWN,
-//        _______, _______, _______, BL_DOWN
-//    )
-
 //On this keymap, usb is on the right side
 //First row is right column
 
     [_BASE] = LAYOUT_ortho_4x6(
-        _______, _______, _______, _______,_______, TO(_BASE),
-        _______, _______, _______, _______,_______, TO(_MOUSE),
-        _______, _______, _______, _______,_______, TO(_LIGHTS),
-        _______, _______, _______, _______,_______, _______
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DF(_BASE),
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DF(_MOUSE),
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DF(_LIGHTS),
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
     [_MOUSE] = LAYOUT_ortho_4x6(
-        MS_BTN1, MS_UP  , MS_BTN2, MS_WHLU,_______, TO(_BASE),
-        MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD,_______, TO(_MOUSE),
-        _______, _______, _______, _______,_______, TO(_LIGHTS),
-        _______, _______, _______, _______,_______, _______
+        MS_BTN1, MS_UP  , MS_BTN2, MS_WHLU, KC_NO, DF(_BASE),
+        MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD, KC_NO, DF(_MOUSE),
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, DF(_LIGHTS),
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO
     ),
 
     [_LIGHTS] = LAYOUT_ortho_4x6(
-        BL_ON  , _______, BL_OFF , BL_UP  ,_______, TO(_BASE),
-        _______, _______, _______, BL_DOWN,_______, TO(_MOUSE),
-        _______, _______, _______, _______,_______, TO(_LIGHTS),
-        _______, _______, _______, _______,_______, _______
+        BL_ON, KC_NO, BL_OFF, BL_UP  , KC_NO, DF(_BASE),
+        KC_NO, KC_NO, KC_NO,  BL_DOWN, KC_NO, DF(_MOUSE),
+        KC_NO, KC_NO, KC_NO,  KC_NO,   KC_NO, DF(_LIGHTS),
+        KC_NO, KC_NO, KC_NO,  KC_NO,   KC_NO, KC_NO
     )
 };
+
+#define LED D0
+#define WAIT 100
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+		if (record->event.pressed)
+		{
+			return true;
+		}
+		
+		switch (keycode) {
+		case DF(_BASE):
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			return true;
+		
+		case DF(_MOUSE):
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			return true;
+		
+		case DF(_LIGHTS):
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			wait_ms(WAIT);
+			gpio_toggle_pin(LED);
+			return true;
+		
+		default:
+			return true; // Process all other keycodes normally
+  }
+}
